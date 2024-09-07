0f64bcabc5a6e76e69efd7a51e4ee31c91939bbe
Ignacy Świderski <ignacyswiderski@wp.pl>
2024-09-07 21:57:38 +0200

    Register new models in admin

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tool, ToolTag, Rental, Lender, LenderTeam
 
 # Register your models here.
+
+admin.site.register(Tool)
+admin.site.register(ToolTag)
+admin.site.register(Rental)
+admin.site.register(Lender)
+admin.site.register(LenderTeam)
